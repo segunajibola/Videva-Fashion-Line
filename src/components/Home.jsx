f307812749beab688cb313f59a021bd38e2d2ccc
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-14 12:08:24 +0100

    addVite

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
+    <>
     <header className="pt-4 md:pt-0 px-2 z-10 md:flex min-h-full items-center justify-between w-full bg-gray-100">
       <div className="w-[98vw] mx-auto">
         <div className="md:flex md:space-x-10 justify-between pt-4 px-1 md:px-2 md:pt-10 md:h-[80vh] mx-auto">
@@ -29,6 +30,21 @@ const Home = () => {
         </div>
       </div>
     </header>
+
+    <section className="mt-10 pt-5 h-full" style={{backgroundImage: `url(images/bgfashion.png)`}} id="about">
+        <div className="py-10 px-3 text-xl">
+            <h2 className="text-4xl text-center pb-3 border-b-4">About Us</h2>
+            <div className="mt-10 -ml-1 space-y-6">
+              <p className=""><span className="p-1 border top-8 -mb -10 mr-2 inline rounded-full">1</span>DIY Sewing Community</p>
+              <p className=""><span className="p-1 border top-8 -mb-10 mr-2 inline rounded-full">2</span>Fashion Students</p>
+              <p className=""><span className="p-1 border top-8 -mb-10 mr-2 inline rounded-full">3</span>Clothing Boutiques</p>
+              <p className=""><span className="p-1 border top-8 -mb-10 mr-2 inline rounded-full">4</span>Tailors & Dressmakers</p>
+              <p className=""><span className="p-1 border top-8 -mb-10 mr-2 inline rounded-full">5</span>Training</p>
+              <p className=""><span className="p-1 border top-8 -mb-10 mr-2 inline rounded-full">6</span>Consulting</p>
+            </div>
+        </div>
+    </section>
+    </>
   );
 };
 
