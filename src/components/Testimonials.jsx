fe9422a8596d390ad8cdaf1827498bdad04c8ade
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-23 22:35:22 +0100

    editSection

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Testimonials = () => {
   return (
-    <section className="bg-gray-200 dark:bg-gray-100 text-gray-900 px-20 py-32 text-lg z-10 relative">
+    <section className="bg-gray-200 dark:bg-gray-100 text-gray-900 px-20 py-12 text-lg z-10 relative">
       <div>
         <h3 className="text-center text-3xl mb-12">Testimonials</h3>
-        <div className="grid grid-cols-3 w-[80vw] h-[50vh]">
+        <div className="grid grid-cols-3 w-[80vw] h-[27rem] mx-auto border-4 border-red-500">
             <div className="col-span-1 border-4 border-red-500">
-                <img src="/images/corey.jpg" alt="" className="h-[26rem] flex align-" />
+                <img src="/images/corey.jpg" alt="" className="h-[26rem] ml-auto" />
             </div>
-            <div className="relative col-span-2">
-                <div className="absolute top-40">
-                   <div className="bg-gray-900 rounded-md w-32 h-32 left-0">
+            <div className="relative col-span-2 border-4 border-red-500">
+                <div className="absolute top-40 -left-2">
+                   <div className="bg-gray-900 rounded-lg w-32 h-32 left-0">
                     <h3 className="text-white">Jake Smith</h3>
                 </div> 
                 </div>
