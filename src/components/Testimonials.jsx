ef881d6d13f56280296c2de9792ada20482dca82
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-23 22:44:10 +0100

    editSection

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import testimonials from "../data/testimonials";
 
 const Testimonials = () => {
   return (
@@ -6,17 +7,19 @@ const Testimonials = () => {
       <div>
         <h3 className="text-center text-3xl mb-12">Testimonials</h3>
         <div className="grid grid-cols-3 w-[80vw] h-[27rem] mx-auto border-4 border-red-500">
-            <div className="col-span-1 border-4 border-red-500">
-                <img src="/images/corey.jpg" alt="" className="h-[26rem] ml-auto" />
+          <div className="col-span-1 border-4 border-red-500">
+            <img src="/images/corey.jpg" alt="" className="h-[26rem] ml-auto" />
+          </div>
+          <div className="relative col-span-2 border-4 border-red-500">
+            <div className="absolute top-40 -left-2">
+              <div className="bg-gray-900 rounded-lg w-32 h-32 left-0">
+                {testimonials.map((testimonial) => (
+                  <div>{testimonial[0].text}</div>
+                ))}
+                <h3 className="text-white">Jake Smith</h3>
+              </div>
             </div>
-            <div className="relative col-span-2 border-4 border-red-500">
-                <div className="absolute top-40 -left-2">
-                   <div className="bg-gray-900 rounded-lg w-32 h-32 left-0">
-                    <h3 className="text-white">Jake Smith</h3>
-                </div> 
-                </div>
-                
-            </div>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/data/testimonials.js
@@ -0,0 +1,28 @@
+const testimonials = [
+  {
+    id: 1,
+    image: "/images/corey.jpg",
+    alt: "Videva Fashion Line",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.",
+    name: "John Doe",
+    position: "Owner, VFL",
+  },
+  {
+    id: 2,
+    image: "/images/corey.jpg",
+    alt: "Videva Fashion Line",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.",
+    name: "John Doe",
+    position: "Staff Engineer, ABL",
+  },
+  {
+    id: 3,
+    image: "/images/corey.jpg",
+    alt: "Videva Fashion Line",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci maxime eligendi tempora tempore, consequuntur nihil, libero tenetur sed nesciunt, unde accusantium! Ea sint saepe quaerat quia est eius fugit.",
+    name: "John Doe",
+    position: "Staff Engineer, ABL",
+  },
+];
+
+export default testimonials;
