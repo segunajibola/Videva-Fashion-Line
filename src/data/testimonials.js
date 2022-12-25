31e693f6350d9764d96637a68a0e543315db451a
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-26 00:04:23 +0100

    editSection

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -4,35 +4,32 @@ const testimonials = [
     image: "/images/testimonial1.jpg",
     alt: "Videva Fashion Line",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci.",
-    name: "John Doe",
+    name: "Olivia",
     position: "Owner, VFL",
     rating: 2
   },
   {
     id: 2,
     image: "/images/testimonial2.jpg",
-    alt: "Videva Fashion Line",
+    alt: "Ava",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci.",
-    name: "John Doe",
-    position: "Staff Engineer, ABL",
+    name: "Ava",
     rating: 3
   },
   {
     id: 3,
     image: "/images/testimonial3.jpg",
-    alt: "Videva Fashion Line",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci.",
-    name: "John Doe",
-    position: "Staff Engineer, ABL",
+    alt: "Sophia",
+    text: "I love that I don't have to worry about what to wear anymore.",
+    name: "Sophia",
     rating: 4
   },
   {
     id: 4,
     image: "/images/testimonial4.jpg",
-    alt: "Videva Fashion Line",
+    alt: "Alice",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum adipisci.",
-    name: "John Doe",
-    position: "Staff Engineer, ABL",
+    name: "Alice",
     rating: 3
   },
 ];
