abd2568da41c83c1ca972c66fdf5c86312494ffd
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-26 02:38:58 +0100

    editSection

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,32 +8,32 @@ const Contact = () => {
       <form
         method="POST"
         action="https://formsubmit.co/cfd3b6bc430690ebff065253cdfdb7b5"
-        className="grid grid-cols-2 grid-rows-2 gap-4 max-w-[600px] w-full -mt-5 sm:mt-20 md:mt-16 bg-red-200"
+        className="grid grid-cols-2 grid-rows-3 gap-4 max-w-[600px] w-full -mt-5 sm:mt-20 md:mt-16"
       >
-        <div className="border-4 bg-green-50 border-blue-200">
+        {/* <div className=""> */}
           <input
-            className="bg-gray-400 dark:bg-gray-300 text-gray-900 rounded-lg p-2 outline-none placeholder:text-gray-200 m-2 border-4"
+            className="bg-gray-400 dark:bg-gray-300 text-gray-900 rounded-lg p-2 outline-none placeholder:text-gray-200 m-2 border-4 h-8"
             type="text"
             placeholder="Name"
             name="name"
             required
           />
-        </div>
+        {/* </div> */}
 
-        <div className="border-4 border-blue-200 bg-green-50">
+        {/* <div className="border-4 border-blue-200 bg-green-50 h-16"> */}
           <input
-            className="bg-gray-400 dark:bg-gray-300 p-2 text-gray-800 rounded-lg outline-none placeholder:text-gray-200 m-2"
+            className="bg-gray-400 dark:bg-gray-300 p-2 text-gray-800 rounded-lg outline-none placeholder:text-gray-200 m-2 h-8"
             type="email"
             placeholder="Email"
             name="email"
             required
           />
-        </div>
-        <div className="row-start-2  border-4">
+        {/* </div> */}
+        <div className="row-start-2 col-span-2 border-4">
           <textarea
             className="bg-gray-400 dark:bg-gray-300 text-gray-800 rounded-lg p-2 outline-none placeholder:text-gray-200 row-start-2  border-4"
             name="message"
-            rows="10"
+            rowds="10"
             placeholder="Message"
             required
           ></textarea>
@@ -45,29 +45,9 @@ const Contact = () => {
           value="https://www.segunajibola.com/thankyou"
         />
         <input type="hidden" name="_captcha" value="false" />
-        <button className="bg-gray-400 dark:bg-gray-300 text-gray-100 border-2 hover:text-gray-800 hover:bg-gray-100 hover:border-gray-200 px-4 py-3 my-8 mx-auto flex items-center dark:text-gray-900 dark:border-gray-500 dark:hover:bg-gray-300 dark:hover:border-gray-800">
+        <button className="bg-gray-400 dark:bg-gray-300 text-gray-100 border-2 hover:text-gray-800 hover:bg-gray-100 hover:border-gray-200 px-4 py-3 my-8 mx-auto flex items-center dark:text-gray-900 dark:border-gray-500 dark:hover:bg-gray-300 dark:hover:border-gray-800 row-start-3 col-span-2">
           Send mail
         </button>
-      </form>
-
-      <form class="grid grid-cols-2 grid-rows-2 gap-4 max-w-[600px] w-full -mt-5 sm:mt-20 md:mt-16 bg-red-200 relative">
-      <div className="border-4 bg-green-50 border-blue-200">
-          <input
-            className="bg-gray-400 dark:bg-gray-300 text-gray-900 rounded-lg p-2 outline-none placeholder:text-gray-200 m-2 border-4"
-            type="text"
-            placeholder="Name"
-            name="name"
-            required
-          />
-        </div>
-        <div className="bg-blue-100">01</div>
-        <div className="bg-blue-100">01</div>
-        <div className="bg-blue-100">01</div>
-        <div className="bg-blue-100">01</div>
-        <div className="bg-blue-100">01</div>
-        <div className="bg-blue-100">01</div>
-        <div className="bg-blue-100">01</div>
-        <div className="bg-blue-100">01</div>
       </form>
     </section>
   );
