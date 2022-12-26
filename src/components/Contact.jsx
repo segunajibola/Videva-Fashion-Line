8331bec4aff8bfb876e460b8cdab75269fb0c291
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-26 01:39:19 +0100

    editSection

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,29 +8,37 @@ const Contact = () => {
       <form
         method="POST"
         action="https://formsubmit.co/cfd3b6bc430690ebff065253cdfdb7b5"
-        className="flex cols-span-2 row-span-2 max-w-[600px] w-full -mt-5 sm:mt-20 md:mt-16"
+        className="grid grid-cols-2 grid-rows-2 gap-4 max-w-[600px] w-full -mt-5 sm:mt-20 md:mt-16"
       >
-        <input
-          className="bg-gray-400 dark:bg-gray-300 text-gray-900 rounded-lg p-2 outline-none placeholder:text-gray-200 m-2 h-8"
+        <div className="border-4 bg-red-200">
+          <input
+          className="bg-gray-400 dark:bg-gray-300 text-gray-900 rounded-lg p-2 outline-none placeholder:text-gray-200 m-2  border-4"
           type="text"
           placeholder="Name"
           name="name"
           required
         />
-        <input
+        </div>
+        
+        <div className="border-4 h-32">
+          <input
           className="bg-gray-400 dark:bg-gray-300 p-2 text-gray-800 rounded-lg outline-none placeholder:text-gray-200 m-2 h-8"
           type="email"
           placeholder="Email"
           name="email"
           required
         />
-        <textarea
-          className="bg-gray-400 dark:bg-gray-300 text-gray-800 rounded-lg p-2 outline-none placeholder:text-gray-200 row-start-2"
+        </div>
+        <div className="row-start-2  border-4">
+          <textarea
+          className="bg-gray-400 dark:bg-gray-300 text-gray-800 rounded-lg p-2 outline-none placeholder:text-gray-200 row-start-2  border-4"
           name="message"
           rows="10"
           placeholder="Message"
           required
         ></textarea>
+        </div>
+        
         <input
           type="hidden"
           name="_next"
