948513e67798949e10ae9ce5dc1c09c8fafa6b21
Segun Ajibola <ajbl.segun@gmail.com>
2022-12-28 08:59:20 +0100

    editSection

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="bg-gray-900 px-12 py-8 text-gray-100">
-      <div className="grid gap-8">
+      <div className="grid grid-cols-3 gap-8">
         <div>
           <div className="flex">
             <Link
@@ -21,9 +21,27 @@ const Footer = () => {
             </Link>
             <span className="text-xl my-auto">MODA</span>
           </div>
+          <span>+234 8102 5853</span>
+          <span>support@gmal.com</span>
         </div>
-        <div></div>
-        <div></div>
+
+        <div>
+            <h6>Quick Links</h6>
+            <div className="flex gap-12">
+                <span>Home</span>
+                <span>Blog</span>
+            </div>
+            <div className="flex gap-12">
+                <span>Services</span>
+                <span>Contact</span>
+            </div>
+        </div>
+
+        <div>
+            <span>Subscribe</span>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <MdOutlineKeyboardArrowRight />
+        </div>
       </div>
     </footer>
   );
